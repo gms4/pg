5520db22e8bb053c3340acf0550ae6aca9af727b
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2021-10-31 15:48:31 -0300

    modificando questao 7

--- Projeto 1/Q7/sketch.js
+++ Projeto 1/Q7/sketch.js
@@ -3,11 +3,7 @@ const RAD = 6.28319
 const RED = [255, 0, 0]
 const GREEN = [0, 255, 0]
 const BLUE = [0, 0, 255]
-const outer_circle_diam = 100*UC
-const inner_circle_diam = 25*UC
-const indicator_diam = 8*UC
-var outer_angle = 0
-var inner_angle = 0
+const YELLOW = [255, 255, 0]
 
 function setup() {
   createCanvas(600, 600, WEBGL);
@@ -16,18 +12,19 @@ function setup() {
 
 function looping(){
   noFill()
-  beginShape()
-  vertex(400, 75, 0)  // A2
-  vertex(400, 125, 0)  // A1
-  vertex(-100, (50/3)+25, 500)   // B1
-  vertex(0, 25, 600)   // C1
-  vertex(100, (-50/3)+25, 500)   // D1
-  vertex(-400, -75, 0)   //E1
-  vertex(-400, -125, 0)  //E2
-  vertex(100, (-50/3)-25, 500)   // D2
-  vertex(0, -25, 600)   // C2
-  vertex(-100, (50/3)-25, 500)   // B2
-  endShape(CLOSE)
+  stroke(YELLOW)
+  bezier(400,-100,0 , -800,-200,600, 800,200,600, -400,100,0)
+}
+
+function coordLines(){
+  strokeWeight(2)
+  stroke(RED)
+  line(-1000,0,0, 1000,0,0)   //X
+  stroke(GREEN)
+  line(0,1000,0, 0,-1000,0)  //Y
+  stroke(BLUE)
+  line(0,0,0, 0,0,1000)   //Z
+  stroke(0)
 }
 
 function draw() {
@@ -39,15 +36,11 @@ function draw() {
   rotateZ(-RAD/3)
   rotateZ(RAD/8)
   
-  strokeWeight(2)
-  stroke(255, 0, 0)
-  line(-1000,0,0, 1000,0,0)   //X
-  stroke(0, 255, 0)
-  line(0,1000,0, 0,-1000,0)  //Y
-  stroke(0, 0, 255)
-  line(0,0,-1000, 0,0,1000)   //Z
-  stroke(0)
-
+  coordLines()
   looping()
+
+  translate(400,-100,0)
+  normalMaterial()
+  sphere(20*UC)
   
 }
